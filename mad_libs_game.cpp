8e0de9a21e415605d743c9bbd5563070e6c99811
paarthsaggarwal <79054391+paarthsaggarwal@users.noreply.github.com>
2022-04-11 17:06:10 +0100

    Update mad_libs_game.cpp

--- mad_libs_game.cpp
+++ mad_libs_game.cpp
@@ -1,16 +1,23 @@
 # include <iostream>
+# include <cmath>
 
 using namespace std;
 
 int main()
 {
-    string color, pluralNoun, personlove, drug, animal;
+    string noun1, color, pluralNoun, adjective1, personlove, drug, animal, moviecharacter, adjective2;
+
+    cout << "Enter a common noun: ";
+    getline(cin, noun1);
 
     cout << "Enter a color: ";
     getline(cin, color);
 
     cout << "Enter a plural noun: ";
     getline(cin, pluralNoun);
+
+    cout << "Enter an adjective: ";
+    getline(cin, adjective1);
 
     cout << "Enter a person who you love: ";
     getline(cin, personlove);
@@ -21,13 +28,19 @@ int main()
     cout << "Enter an animal: ";
     getline(cin, animal);
 
-    cout << "Houses are " << color << endl;
-    cout <<  pluralNoun << " are vicious" << endl;
+    cout << "Enter a movie character: ";
+    getline(cin, moviecharacter);
+
+    cout << "Enter an adjective: ";
+    getline(cin, adjective2);
+
+    cout << noun1 << " are so " << color << endl;
+    cout <<  pluralNoun << " are " << adjective1 << endl;
     cout << "I love " << personlove << endl;
     cout << "I don't take " << drug << endl;
     cout << "Have you seen this " << animal << "?" << endl;
+    cout << "The last movie featuring " << moviecharacter << " was so " << adjective2;
 
 
     return 0;
 }
-
